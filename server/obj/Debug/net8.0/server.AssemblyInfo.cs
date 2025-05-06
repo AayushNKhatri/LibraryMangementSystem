b00c05976c02cdea2a79e6c339ac898a57b13b60
Aryan <abhishekaanand@islingtoncollege.edu.com.np>
2025-05-06 18:38:56 +0545

    Initial Hub Commit

--- server/obj/Debug/net8.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net8.0/server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0161498dc6dd1295bad732a260d2196e0b5e004")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f95c141069421565954108e57410e7f7633b82bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
