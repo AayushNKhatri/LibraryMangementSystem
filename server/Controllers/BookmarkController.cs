f5a383bd00a92d850a13cb8ef0fa02c86e4802f7
AayushNathKhatri <aayushnathkhatri2004@gmail.com>
2025-05-12 15:25:08 +0545

    Added Review and bookmark services

---
+++ server/Controllers/BookmarkController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using server.Services;
+using server.Services.Interface;
+
+namespace server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookmarkController(IBookmarkServices _bookmarkServices) : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddToBookmark(Guid bookId){
+            try
+            {
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                bool book = await _bookmarkServices.AddBookmark(bookId, userId);
+                if(!book)
+                   throw new Exception("Cannot add to bookmark");
+
+                return Ok("Sucesfully added bookmark");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+
+            }
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteBookmark(Guid bookmarkId){
+            try{
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var book = await _bookmarkServices.DeleteBookmark(bookmarkId);
+                return Ok("Sucesfully Deleted");
+            }
+            catch{
+                throw new Exception("Cant add bookmark");
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllBookmark(){
+            try{
+                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var bookmark = await _bookmarkServices.GetBookmark(userId);
+                return Ok(bookmark);
+            }
+            catch(Exception ex){
+                throw new Exception(ex.Message,ex);
+            }
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetBookmarkById(Guid bookmarkId){
+            try{
+                var bookmark = await _bookmarkServices.GetBookmarkByID(bookmarkId);
+                return Ok(bookmark);
+            }
+            catch(Exception ex){
+                throw new Exception(ex.Message, ex);
+            }
+        }
+    }
+}
